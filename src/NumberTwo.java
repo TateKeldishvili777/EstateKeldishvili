11cae7e275dae8a59004673907daab94b48515aa
Tate777331 <estate.keldishvili.1@btu.edu.ge>
2022-07-12 20:42:50 +0400

    number two

---
+++ src/NumberTwo.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class NumberTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int amount = scanner.nextInt();
+
+        System.out.println(minSplit(amount));
+    }
+
+    public static int minSplit(int amount) {
+        int answer = 0;
+        while (amount >= 50) {
+            amount -= 50;
+            answer += 1;
+        }
+        while (amount >= 20) {
+            amount -= 20;
+            answer += 1;
+        }
+        while (amount >= 10) {
+            amount -= 10;
+            answer += 1;
+        }
+        while (amount >= 5) {
+            amount -= 5;
+            answer += 1;
+        }
+        while (amount > 0) {
+            amount -= 1;
+            answer += 1;
+        }
+
+
+        return answer;
+    }
+}
