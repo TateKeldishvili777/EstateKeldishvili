f88dece11c66993c93f41dd8e147bed8a08a6490
Tate777331 <estate.keldishvili.1@btu.edu.ge>
2022-07-12 21:10:27 +0400

    number four

---
+++ src/NumberFour.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class NumberFour {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String sequence = scanner.next();
+
+        System.out.println(isProperly(sequence));
+    }
+
+    public static boolean isProperly(String sequence){
+
+        int amount = 0;
+
+        for(int x=0;x<sequence.length();x++){
+
+            if(sequence.charAt(x)=='('){
+                amount+=1;
+            }
+            if(sequence.charAt(x)==')'){
+                amount-=1;
+            }
+            if(amount<0){
+                break;
+            }
+        }
+
+        return amount == 0;
+    }
+}
