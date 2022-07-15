fde5cc8b3f4b9af3bc812118adfc9d3328903c99
Tate777331 <estate.keldishvili.1@btu.edu.ge>
2022-07-15 11:39:09 +0400

    number 7 tables created

--- src/NumberSeven.java
+++ src/NumberSeven.java
@@ -1,26 +1,70 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class NumberSeven {
-    public static void main(String[] args) throws Exception {
 
-        getConnection();
-    }
+    public static void main(String[] args) {
 
-    public static Connection getConnection() throws Exception{
-        try{
-            String driver = "com.mysql.jdbc.Driver";
-            String url = "jdbc:mysql://24.196.52.166:3306/mydb";
+        Connection connection = null;
+        try {
+
+
+            String driverName = "com.mysql.jdbc.Driver";
+
+            Class.forName(driverName);
+
+
+            String serverName = "localhost";
+
+            String schema = "mydb";
+
+            String url = "jdbc:mysql://" + serverName + "/" + schema;
+
             String username = "root";
+
             String password = "Qwdfbn@@11";
-            Class.forName(driver);
 
-            Connection conn = DriverManager.getConnection(url,username,password);
-            System.out.println("Connected");
-            return conn;
-        } catch(Exception e){System.out.println(e);}
+            connection = DriverManager.getConnection(url, username, password);
 
 
-        return null;
+            System.out.println("Successfully Connected to the database!");
+
+
+        } catch (ClassNotFoundException e) {
+
+            System.out.println("Could not find the database driver " + e.getMessage());
+        } catch (SQLException e) {
+
+            System.out.println("Could not connect to the database " + e.getMessage());
+        }
+
+        try {
+
+            Statement statement = connection.createStatement();
+
+
+            statement.executeUpdate("CREATE TABLE Teacher(FirstName varchar(20)," +
+                    "LastName varchar(30)," +
+                    "Gender varchar(10)," +
+                    "Subject varchar(20))");
+
+
+            System.out.println("Successfully created Teacher table");
+
+            statement.executeUpdate("CREATE TABLE Pupil(FirstName varchar(20)," +
+                    "LastName varchar(30)," +
+                    "Gender varchar(10)," +
+                    "Class int)");
+
+
+            System.out.println("Successfully created Pupil table");
+
+        } catch (SQLException e) {
+
+            System.out.println("Could not create the database table " + e.getMessage());
+        }
+
     }
 }
